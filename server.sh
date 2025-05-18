6b5742e148ea8d32769d38e06ea9f0b2f0983c3d
Nepaligamingcommunity <113175557+Nepaligamingcommunity@users.noreply.github.com>
2025-05-18 19:30:38 +0545

    Update server.sh

--- server.sh
+++ server.sh
@@ -4,3 +4,6 @@ if [ -f start.sh ]; then
 else
     echo "File start.sh does not exist."
 fi
+
+echo "🚀 Launching Minecraft server..."
+java -Xms5G -Xmx10G -jar server.jar --nogui || { echo "❌ Server failed to start. Check logs."; exit 1; }
