2c72605369642605e0e83a4d37ea43d7f1b2703d
Nepaligamingcommunity <113175557+Nepaligamingcommunity@users.noreply.github.com>
2025-05-18 18:37:29 +0545

    server.sh

--- server.sh
+++ server.sh
@@ -1,2 +1,6 @@
-echo "🚀 Launching Minecraft server..."
-java -Xms5G -Xmx10G -jar server.jar --nogui || { echo "❌ Server failed to start. Check logs."; exit 1; }
+if [ -f start.sh ]; then
+    rm start.sh
+    echo "Deleted start.sh"
+else
+    echo "File start.sh does not exist."
+fi
